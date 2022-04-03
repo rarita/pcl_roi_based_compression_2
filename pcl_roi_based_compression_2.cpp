55bbdbcac17f0dba7117b33ddfbba50b37ecb426
Анна Григорьева <310349@niuitmo.ru>
2022-04-03 21:08:43 +0300

    Параллелизм omp, плотность распределения в float32 поле Intensity

--- pcl_roi_based_compression_2.cpp
+++ pcl_roi_based_compression_2.cpp
@@ -7,19 +7,23 @@ using namespace std;
 
 static const std::string ORIGIN_PATH = "F:/repos/prbc2/pcl_roi_based_compression_2/clouds";
 
-static const std::string PLY_PATH = ORIGIN_PATH + "/bunny/work/onplane_noisy_2.ply";
-static const std::string FILE_SAVE_PATH = ORIGIN_PATH + "/bunny/processed.ply";
+static const std::string PLY_PATH = ORIGIN_PATH + "/reak/01_ss.ply";
+// Путь и имя файла без расширения для дописывания разных суффиксов (bw)
+static const std::string FILE_SAVE_PATH = ORIGIN_PATH + "/reak/processed/processed";
 
 /**
 * Создаем плоскую двумерную поверхность с заданными параметрами
 * и сохраняем как облако точек в формате .ply
 */
-void generateSurface(std::string fname, int dim, double spacing, double noisePower, double tileOffset) {
+void generateSurface(
+	std::string fname, int dim, double spacing, 
+	double noisePower, double tileOffset, int octaves) {
 
 	pcl::PointCloud<pcl::PointXYZ> surf;
 	surf.resize(dim, dim);
 
 	noise::module::Perlin perlin;
+	perlin.SetOctaveCount(octaves);
 	noise::utils::NoiseMap heightMap; 
 	noise::utils::NoiseMapBuilderPlane heightMapBuilder;
 	heightMapBuilder.SetSourceModule(perlin);
@@ -46,24 +50,13 @@ void generateSurface(std::string fname, int dim, double spacing, double noisePow
 
 }
 
-void processCloud() {
+void _processCloudInternal(int bw, pcl::PointCloud<pcl::PointXYZI>::Ptr cloud) {
 
-	// Считываем облако из .ply файла
-	// Тип точек - XYZL (X, Y, Z, Label). Считаем, что Label = 0 по дефолту.
-	pcl::PointCloud<pcl::PointXYZL>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZL>);
-	pcl::PLYReader Reader;
-	int res = Reader.read(PLY_PATH, *cloud);
-	assert(res == 0);
-	std::cout << "Successfully read point cloud at " << PLY_PATH << std::endl;
-
-	// Обучим модель KDE на точках облака
-	// Если облако слишком маленькое или слишком большое, возможны неадекватные значения
-	// Нужно выставлять верный размер ядра.
-	const double bandwidth = 1.25;
+	std::cout << "Started processing KDE with bw: " << bw << std::endl;
 	mlpack::kde::KDE<mlpack::kernel::EpanechnikovKernel> kde(
 		mlpack::kde::KDEDefaultParams::relError,
 		mlpack::kde::KDEDefaultParams::absError,
-		mlpack::kernel::EpanechnikovKernel(bandwidth)
+		mlpack::kernel::EpanechnikovKernel((bw * 1.0) / 100)
 	);
 	arma::mat kdeRefData(cloud->size(), 3, arma::fill::none);
 
@@ -89,19 +82,59 @@ void processCloud() {
 	for (int idx = 0; idx < kdeDataEst.n_rows; idx++) {
 		auto& point = (*cloud)[idx];
 		double _val = kdeDataEst(idx);
-		point.label = _val * 100;
+		point.intensity = _val;
 	}
 
 	// Сохраняем обработанное облако (облака) в файл(ы) 
-	pcl::io::savePLYFile(FILE_SAVE_PATH, *cloud, true);
-	std::cout << "Result saved to " << FILE_SAVE_PATH << std::endl;
+	const std::string path = FILE_SAVE_PATH + "-bw" + std::to_string(bw) + ".ply";
+	pcl::io::savePLYFile(path, *cloud, true);
+	std::cout << "Result saved to " << path << std::endl;
+	std::cout << "Done processing KDE with bw: " << bw << std::endl;
 
 }
 
-int main()
-{
+void processCloud() {
 
-	//generateSurface(ORIGIN_PATH + "/surf.ply", 140, 0.033, 0.35, 1.);
+	// Считываем облако из .ply файла
+	// Тип точек - XYZL (X, Y, Z, Label). Считаем, что Label = 0 по дефолту.
+	// todo надо сделать свой ТИП вроде PointXYZI, который в label пишет float64
+	pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+	pcl::PLYReader Reader;
+	int res = Reader.read(PLY_PATH, *cloud);
+	assert(res == 0);
+	std::cout << "Successfully read point cloud at " << PLY_PATH << std::endl;
+
+	// Обучим модель KDE на точках облака
+	// Если облако слишком маленькое или слишком большое, возможны неадекватные значения
+	// Нужно выставлять верный размер ядра.
+	const int minBandwidth = 25;
+	const int maxBandwidth = 500;
+	const int step = 20;
+	assert(step < 1);
+	
+	// precise bandwidth
+	#pragma omp parallel for
+	for (int bw = minBandwidth; bw < 125; bw += step) {
+		_processCloudInternal(bw, cloud);
+	}
+
+	// broad bandwidth
+	#pragma omp parallel for
+	for (int bw = 125; bw < maxBandwidth; bw += 100) {
+		_processCloudInternal(bw, cloud);
+	}
+
+}
+
+int main(int argc, char* argv[])
+{
+	/*
+	if (argc != 3) {
+		std::cout << "Program requires 2 arguments ;)";
+		return 1;
+	}
+	*/
+	//generateSurface(ORIGIN_PATH + "/surf.ply", 140, 0.033, 0.25, 3., 3);
 	processCloud();
 	return 0;
 
